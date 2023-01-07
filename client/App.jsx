c511a95560bc8c10fd49502e55d4cc6cd2c82283
Anne Blazejack <annieblazejack@gmail.com>
2023-01-07 18:07:20 -0500

    Saturday, Skeleton of React Pages

--- client/App.jsx
+++ client/App.jsx
@@ -1,28 +1,37 @@
-import React, {useState, useEffect} from 'react';
-import ReactDOM from 'react-dom/client';
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
+import Homescreen from "./Pages/homescreen";
+import StoryPage from "./Pages/storypage";
+import AtTheBar from "./Pages/atthebar";
+import BarMap from "./Pages/map";
 
 export default function App() {
-  
-  useEffect( ()=> {
-    const showSomething = async () => {
-     await fetch("/test") //ask server to serve up all tamas from db
-      .then((data) => data.json())
-      .then((data) => {
-        console.log(data);
-      })
-      .catch((error) => {
-        console.log("Error:", error);
-      });
-    }
+  // useEffect( ()=> {
+  //   const showSomething = async () => {
+  //    await fetch("/test") //ask server to serve up all tamas from db
+  //     .then((data) => data.json())
+  //     .then((data) => {
+  //       console.log(data);
+  //     })
+  //     .catch((error) => {
+  //       console.log("Error:", error);
+  //     });
+  //   }
 
-    showSomething();
+  //   showSomething();
 
-  }, [])
+  // }, [])
 
-  return(
+  return (
     <div>
-      <p>This is a paragraph!</p>
+      {/* <Switch> */}
+      <Route path="/" exact component={Homescreen} />
+      <Route path="/atthebar" exact component={AtTheBar} />
+      <Route path="/barmap" exact component={BarMap} />
+      <Route path="/storypage" exact component={StoryPage} />
+      {/* </Switch> */}
     </div>
-  )
+  );
 }
